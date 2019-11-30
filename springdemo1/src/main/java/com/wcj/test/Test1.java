189b8d61b3135678e12c3b1307c432000a849e16
wangyang005 <wangchengjiang10@163.com>
2019-11-30 20:46:02 +0800

    aaa
    bbbb

--- springdemo1/src/main/java/com/wcj/test/Test1.java
+++ springdemo1/src/main/java/com/wcj/test/Test1.java
@@ -2,6 +2,6 @@ package com.wcj.test;
 
 public class Test1 {
 	public static void main(String[] args) {
-		System.out.println("this is a test");
+		System.out.println("this is a test 2222222");
 	}
 }
