fe0b455ff9a4e82d7d9fea7c98c4216e5f00b83f
wcj <wangchengjiang10@163.com>
2019-11-30 21:00:47 +0800

    第三次提交

--- springdemo1/src/main/java/com/wcj/test/Test1.java
+++ springdemo1/src/main/java/com/wcj/test/Test1.java
@@ -3,5 +3,6 @@ package com.wcj.test;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("this is a test 2222222");
+		System.out.println("this is a test 33322");
 	}
 }
